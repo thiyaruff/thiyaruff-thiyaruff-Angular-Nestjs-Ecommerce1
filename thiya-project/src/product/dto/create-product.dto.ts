520df3b65126aec518cb38396d1b42f08d2a0306
thiyaruff <thiyaruff@gmail.com>
2023-07-19 20:46:24 +0300

    first commit

---
+++ thiya-project/src/product/dto/create-product.dto.ts
@@ -0,0 +1,37 @@
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    title:string;
+
+    @IsString()
+    description:string;
+
+    @IsString()
+    imgUrl1:string;
+    
+    @IsNumber()
+    price:number;
+
+    @IsNumber()
+    quantity:number;
+
+    @IsString()
+    size:string;
+
+    @IsString()
+    color:string;
+
+    @IsString()
+    shippings:string;
+
+    @IsString()
+    gender:string;
+
+    @IsString()
+    brands:string;
+
+    @IsString()
+    category:string;
+
+}
---
+++ thiya-project/src/product/product.service.ts
@@ -0,0 +1,88 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/product.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private productRepository: Repository<ProductEntity>
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+    const newProduct = await this.productRepository.create({...createProductDto})
+
+    await this.productRepository.save(newProduct)
+
+    return newProduct;
+  }
+
+  async findAll() {
+    return await this.productRepository.find()
+  } 
+
+  async findOne(id: string) {
+    const findProduct = await this.productRepository.findOne({where:{id:id}})
+    
+    if (!findProduct){
+      throw new HttpException('No product found', HttpStatus.NOT_FOUND)
+    }
+
+    return findProduct;
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const
+     {title,
+    description,
+    imgUrl1,
+    price,
+    quantity,
+    size,
+    color,
+    shippings,
+    gender,
+    brands,
+    category} = updateProductDto;
+
+    const findProduct = await this.productRepository.findOne({where:{id:id}})
+    
+    if (!findProduct){
+      throw new HttpException('No product found', HttpStatus.NOT_FOUND)
+    }
+
+    let updateProduct:any ={}
+
+    title && (updateProduct.title=title);
+    description && (updateProduct.description=description);
+    imgUrl1 && (updateProduct.imgUrl1=imgUrl1);
+    price && (updateProduct.price=price);
+    quantity && (updateProduct.quantity=quantity);
+    size && (updateProduct.size=size);
+    color && (updateProduct.color=color);
+    shippings && (updateProduct.shippings=shippings);
+    gender && (updateProduct.gender=gender);
+    brands && (updateProduct.brands=brands);
+    category && (updateProduct.category);
+
+    await this.productRepository.update({id:id}, updateProduct)
+
+    const findProductAgain =await this.productRepository.findOne({where:{id:id}})
+
+    return findProductAgain;
+  }
+
+  async remove(id: string) {
+    const findProduct = await this.productRepository.findOne({where:{id:id}})
+    
+    if (!findProduct){
+      throw new HttpException('No product found', HttpStatus.NOT_FOUND)
+    }
+    await this.productRepository.remove(findProduct)
+
+    return 'This product has deleted'
+  }
+}
